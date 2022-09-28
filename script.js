9784a1288c87b8a02bce3d08530aa45ecc57da02
GabrielDSanta <eloviveiros@hotmail.com>
2022-09-27 22:03:45 -0300

    Adicionando Encapsulamento

--- script.js
+++ script.js
@@ -6,62 +6,238 @@ let Passagens = []
 let Pacotes = []
 
 class Cliente {
-    Nome
-    CPF
-    DataNascimento
+    #Nome
+    #CPF
+    #DataNascimento
 
     constructor(nome, cpf, dataNascimento){
-        this.Nome = nome
-        this.CPF = cpf
-        this.DataNascimento = dataNascimento
+        this.DefinirNome(nome)
+        this.DefinirCPF(cpf)
+        this.DefinirDataNascimento(dataNascimento)
     }
+
+    DefinirNome(nome){
+        if(nome.length > 50){
+            return alert("Cadastre um nome com menos de 50 caracteres")
+        }
+
+        else{
+            return this.#Nome = nome
+        }
+    }
+
+    DefinirCPF(cpf){
+        if(cpf.length > 11){
+            return alert("CPF inválido")
+        }
+
+        else{
+            return this.#CPF = cpf
+        }
+    }
+
+    DefinirDataNascimento(dataNascimento){
+        return this.#DataNascimento = dataNascimento
+    }
+
+    ExibirNomeCliente(){
+        return alert(`${this.#Nome}`)
+    }
+
+    ExibirCPFCliente(){
+        return alert(`${this.#CPF}`)
+    }
+
+    ExibirDataCliente(){
+        return alert(`${this.#DataNascimento}`)
+    }
+
+    ExibirDadosCliente(){
+        return alert(`Nome: ${this.#Nome} CPF: ${this.#CPF} Data De Nascimento: ${this.#DataNascimento}`)
+    }
+
+
+
+
 }
 
 class PacoteViagem{
-    Titular
-    DataIda
-    DataVolta
-    ValorTotal
+    #Titular
+    #DataIda
+    #DataVolta
+    #ValorTotal
 
     constructor(titular, dataIda, dataVolta, valorTotal){
-        this.Titular = titular
-        this.dataIda = dataIda
-        this.dataVolta = dataVolta
-        this.valorTotal = valorTotal
+        this.DefinirTitular(titular)
+        this.DefinirDataIda(dataIda)
+        this.DefinirDataVolta(dataVolta)
+        this.DefinirValorTotal(valorTotal)
     }
+
+    DefinirTitular(titular){
+        return this.#Titular = titular
+    }
+
+    DefinirDataIda(dataIda){
+        return this.#DataIda = dataIda
+    }
+
+    DefinirDataVolta(dataVolta){
+        return this.#DataVolta = dataVolta
+    }
+
+    DefinirValorTotal(valorTotal){
+        return this.#ValorTotalr = valorTotal
+    }
+
+    ExibirTitular(){
+        return this.#Titular
+    }
+
+    ExibirDataIda(){
+        return this.#DataIda
+    }
+
+    ExibirDataVolta(){
+        return this.#DataVolta
+    }
+
+    ExibirValorTotal(){
+        return this.#ValorTotal
+    }
+
 }
 
 class Voo{
-    Empresa
-    Numero
-    DataVoo
-    Horario
-    LocalPartida
-    LocalDestino
+    #Empresa
+    #Numero
+    #DataVoo
+    #Horario
+    #LocalPartida
+    #LocalDestino
 
     constructor(empresa, numero, dataVoo, horario, localPartida, localDestino){
-        this.Empresa = empresa
-        this.Numero = numero
-        this.DataVoo = dataVoo
-        this.Horario = horario
-        this.LocalPartida = localPartida
-        this.LocalDestino = localDestino
+        this.DefinirEmpresa(empresa)
+        this.DefinirNumero(numero)
+        this.DefinirDataVoo(dataVoo)
+        this.DefinirHorario(horario)
+        this.DefinirLocalPartida(localPartida)
+        this.DefinirLocalDestino(localDestino)
+    }
+
+    DefinirEmpresa(empresa){
+        return this.#Empresa = empresa
+    }
+
+    DefinirNumero(numero){
+        return this.#Numero = numero
+    }
+
+    DefinirDataVoo(dataVoo){
+        return this.#DataVoo = dataVoo
+    }
+
+    DefinirHorario(horario){
+        return this.#Horario = horario
+    }
+
+    DefinirLocalPartida(localPartida){
+        return this.#LocalPartida = localPartida
+    }
+
+    DefinirLocalDestino(localDestino){
+        return this.#LocalDestino = localDestino
+    }
+
+    ExibirEmpresa(){
+        return alert(`${this.#Empresa}`)
+    }
+
+    ExibirNumero(){
+        return alert(`${this.#Numero}`)
+    }
+
+    ExibirDataVoo(){
+        return alert(`${this.#DataVoo}`)
+    }
+
+    ExibirHorario(){
+        return alert(`${this.#Horario}`)
+    }
+
+    ExibirLocalPartida(){
+        return alert(`${this.#LocalPartida}`)
+    }
+
+    ExibirLocalDestino(){
+        return alert(`${this.#LocalDestino}`)
     }
 }
 
 class PassagemArea{
-    Assento
-    PrimeiraClasse
-    Valor
-    Passageiro
-    Voo
+    #Assento
+    #PrimeiraClasse
+    #Valor
+    #Passageiro
+    #Voo
 
     constructor(assento, primeiraClasse, valor, passageiro, voo){
-        this.Assento = assento
-        this.PrimeiraClasse = primeiraClasse
-        this.Valor = valor
-        this.Passageiro = passageiro
-        this.Voo = voo
+        this.DefinirAssento(assento)
+        this.DefinirPrimeiraClasse(primeiraClasse)
+        this.DefinirValor(valor)
+        this.DefinirPassageiro(passageiro)
+        this.DefinirVoo(voo)
+    }
+
+    DefinirAssento(assento){
+        return this.#Assento = assento
+    }
+
+    DefinirPrimeiraClasse(primeiraClasse){
+        if(typeof primeiraClasse == Boolean){
+            return this.#PrimeiraClasse = primeiraClasse
+        }
+
+        else{
+            return
+        }
+    }
+
+    DefinirValor(valor){
+        if(valor > 0){
+            return this.#Valor = valor
+        }
+        else{
+            return 
+        }
+    }
+
+    DefinirPassageiro(passageiro){
+        return this.#Passageiro = passageiro
+    }
+
+    DefinirVoo(voo){
+        return this.#Voo = voo
+    }
+
+    ExibirAssento(){
+        return alert(`${this.#Assento}`)
+    }
+
+    ExibirPrimeiraClasse(){
+        return alert(`${this.#PrimeiraClasse}`)
+    }
+
+    ExibirValor(){
+        return alert(`${this.#Valor}`)
+    }
+
+    ExibirPassageiro(){
+        return alert(`${this.#Passageiro}`)
+    }
+
+    ExibirVoo(){
+        return alert(`${this.#Voo}`)
     }
 
     CalcularValor(){
